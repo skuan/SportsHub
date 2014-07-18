39578852757641c66f52af89f1906d39760ec4ac
skuan <stanykuan@gmail.com>
2014-07-17 19:40:56 -0700

    First version of SportsHub project 07172014

---
+++ app/controllers/sportshub_controller.rb
@@ -0,0 +1,6 @@
+class SportshubController < ApplicationController
+
+	def home
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsHub::Application.config.secret_key_base = 'b1bc225a443070dc323f448e890129693a32e6c2e6d7756fe4a969178cf2f8344ee1807281f5b395442f2f85c46259a2a3e897bbd1654aa2e45bb6052ff40246'
